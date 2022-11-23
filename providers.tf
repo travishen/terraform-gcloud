855ea9b960456608a90c393335be2f448759caa5
travishen <travishen.tw@gmail.com>
2022-11-23 15:00:31 +0800

    Add local, template provider.

--- providers.tf
+++ providers.tf
@@ -12,6 +12,14 @@ terraform {
       source = "hashicorp/tls"
       version = "4.0.4"
     }
+    local = {
+      source = "hashicorp/local"
+      version = "2.2.3"
+    }
+    template = {
+      source = "hashicorp/template"
+      version = "2.2.0"
+    }
   }
   required_version = "1.3.4"
 }
