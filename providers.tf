58c4bab7e4067c623880a60eb446d54eda9581e5
travishen <travishen.tw@gmail.com>
2022-12-20 17:46:49 +0800

    Add providers.

--- providers.tf
+++ providers.tf
@@ -20,6 +20,18 @@ terraform {
       source = "hashicorp/template"
       version = "2.2.0"
     }
+    null = {
+      source = "hashicorp/null"
+      version = "3.2.1"
+    }
+    random = {
+      source = "hashicorp/random"
+      version = "3.4.3"
+    }
+    external = {
+      source = "hashicorp/external"
+      version = "2.2.3"
+    }
   }
   required_version = "1.3.4"
 }
